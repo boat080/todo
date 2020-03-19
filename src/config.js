ff338d168c0571965c7229c850090cafa6f2bfb0
boat080 <boatsg093@gmail.com>
2020-03-19 15:46:43 +0700

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyDRcpe4Ya6o3gn2CNNfo80ZBzDPtaSBz14",
+    authDomain: "todo-ws240311.firebaseapp.com",
+    databaseURL: "https://todo-ws240311.firebaseio.com",
+    projectId: "todo-ws240311",
+    storageBucket: "todo-ws240311.appspot.com",
+    messagingSenderId: "314985452453",
+    appId: "1:314985452453:web:fa6bd092770217f2d78e4c",
+    measurementId: "G-S7M3YGQTXR"
+  };
+export default config
